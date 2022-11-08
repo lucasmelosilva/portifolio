7f3eb92d2140b7e40fb2dff6a2859a90c7b7953e
lucas melo <lucas.melo044@gmail.com>
2022-11-08 12:04:26 -0300

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { NavBar } from "./components/NavBar"
+
 function App() {
 
   return (
-    <h1 className="font-signature text-7xl">Hello</h1>
+    <div>
+      <NavBar />
+    </div>
   )
 }
 
