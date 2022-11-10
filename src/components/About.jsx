388c90d0a4720729ba63c2ff08e83c4677a19457
lucas melo <lucas.melo044@gmail.com>
2022-11-10 08:50:08 -0300

    add about

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aperiam, exercitationem nostrum distinctio porro aliquid maiores quaerat voluptates natus consequatur earum explicabo pariatur minus voluptatem architecto adipisci voluptatum voluptatibus sed. Vel, recusandae animi. Iste itaque expedita dicta odit quae repudiandae, ipsa dolor? Incidunt perspiciatis quia corporis quas omnis maxime itaque.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, quisquam. Possimus, deleniti? Temporibus ratione amet autem cupiditate sunt nesciunt, similique nostrum quas eum numquam iste odio impedit omnis ipsam est veritatis ipsum sapiente vel, beatae accusamus libero, odit eaque! Soluta, labore corrupti nobis deserunt at dolores est! Alias, doloremque sit.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export { About }
