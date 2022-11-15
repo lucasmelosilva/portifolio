d25a0ce104fa6471687baf855bdb5b40388baa9c
lucas melo <lucas.melo044@gmail.com>
2022-11-15 11:43:26 -0300

    add navegacao pelo menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import HeroImage from '../assets/hero-image.jpg'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from "react-scroll"
 
 function Home() {
   return (
@@ -14,12 +15,12 @@ function Home() {
           </p>
 
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
               Portfolio
               <span className="duration-200 group-hover:rotate-90">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link } from 'react-scroll'
 
 function NavBar() {
   const [nav, setNav] = useState(false)
@@ -27,6 +28,9 @@ function NavBar() {
     },
   ]
 
+  function handleNav() {
+    setNav(!nav)
+  }
   return (
     <div className="flex justify-between items-center w-full h-20 bg-black px-4 text-white fixed">
       <div>
@@ -36,12 +40,14 @@ function NavBar() {
       <ul className="hidden md:flex">
         {
           links.map(({ id, link }) => (
-            <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 durantion-200">{link}</li>
+            <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 durantion-200">
+              <Link to={link} smooth duration={500}>{link}</Link>
+            </li>
           ))
         }
       </ul>
 
-      <div onClick={() => setNav(!nav)} className="md:hidden cursor-pointer pr-4 z-10 text-gray-500">
+      <div onClick={handleNav} className="md:hidden cursor-pointer pr-4 z-10 text-gray-500">
 
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
@@ -49,7 +55,9 @@ function NavBar() {
         nav &&
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
-            <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">{link}</li>
+            <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
+              <Link onClick={handleNav} to={link}>{link}</Link>
+            </li>
           ))}
         </ul>
       }
