c87f44d1d293be93f853e12eca2e935208238cf7
lucas melo <lucas.melo044@gmail.com>
2022-11-20 14:09:04 -0300

    add feedback no envio do email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,31 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import emailjs from '@emailjs/browser';
+
 function Contact() {
   const form = useRef()
+  const [status, setStatus] = useState('')
+
+  const statusSpan = {
+    success: {
+      text: "Message sent successfully",
+      style: "bg-blue-400"
+    },
+    err: {
+      text: "Error sending message",
+      style: "bg-red-400"
+    }
+  }
 
   const sendEmail = (e) => {
     e.preventDefault()
+
     emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, import.meta.env.VITE_PUBLIC_ID)
       .then((result) => {
-        console.log(result.text);
+        console.log(result.status)
+        setStatus('success')
       }, (error) => {
         console.log(error.text);
+        setStatus('err')
       });
   }
 
@@ -30,6 +46,13 @@ function Contact() {
             onSubmit={sendEmail}
             className="flex flex-col w-full md:w-1/2 gap-4"
           >
+
+            {
+              status && <div id="message" className={`p-2 rounded-md text-center ${statusSpan[status].style}`}>
+                <p>{statusSpan[status].text}</p>
+              </div>
+
+            }
             <input
               type="text"
               name="name"
@@ -42,14 +65,12 @@ function Contact() {
               name="email"
               placeholder="Enter your email"
               className="p-2 bg-transparent border-2  rounded-md text-white focus:outline-none"
-              required
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-              required
             ></textarea>
 
             <button type="submit" className="group font-medium text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-b from-cyan-500 to-blue-500 hover:scale-105 duration-200 cursor-pointer">Let's talk</button>
